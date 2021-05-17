edcda3013d7542ee18ac567a76cf60b3e0adbd61
Samir-Akhter <sam.akhter007@gmail.com>
2021-05-17 20:34:21 +0530

    v1

---
+++ src/main/java/com/capgemini/CarRentalSystem1Application.java
@@ -0,0 +1,13 @@
+package com.capgemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalSystem1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalSystem1Application.class, args);
+	}
+
+}
